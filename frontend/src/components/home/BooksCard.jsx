47ed8912956b7cd972d1d3edb361310f6fb32967
Somondas <somondas067@gmail.com>
2024-03-08 16:38:51 +0530

    Almost done

---
+++ frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle, BiShow } from "react-icons/bi";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+import { useState } from "react";
+import BookSingleCard from "./BookSingleCard";
+import BookModal from "./BookModal";
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm: grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((item) => (
+        <BookSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  );
+};
+
+export default BooksCard;
